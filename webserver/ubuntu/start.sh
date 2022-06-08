9c412d1ffe549e9a6c0573582bed346749673b18
Yurii Patrin <yury.patrin@gmail.com>
2022-06-08 15:18:14 +0300

    webserver

--- webserver/ubuntu/start.sh
+++ webserver/ubuntu/start.sh
@@ -2,6 +2,10 @@
 
 /usr/sbin/service cron start
 /usr/sbin/service php7.1-fpm start
+/usr/sbin/service php7.2-fpm start
+/usr/sbin/service php7.3-fpm start
+/usr/sbin/service php7.4-fpm start
+/usr/sbin/service php8.1-fpm start
 /usr/sbin/service apache2 start
 
 tail -f /dev/null
